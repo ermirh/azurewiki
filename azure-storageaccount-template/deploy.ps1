ad7bf8f869ad6cc5ad8555c1cbcd1f5371034596
ehoxhaj <hoxhajermir@outlook.com>
2020-03-01 23:05:08 +0100

    sa template final

--- azure-storageaccount-template/deploy.ps1
+++ azure-storageaccount-template/deploy.ps1
@@ -4,9 +4,11 @@ Get-AzSubscription -SubscriptionName '<sub_name>' | Select-AzSubscription
 
 #create new resource group - or use already an existing one (Get-AzResourceGroup)#
 $resourceGroup = "azurewiki-rg-001"
-$templateParamUri = ""
-$templateUri = ""
+$templateParam = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-storageaccount-template\template.parameters.json"
+$template = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-storageaccount-template\template.json"
 
+#test the deployment - empty output is ok, working#
+Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateParameterFile $templateParam -TemplateFile $template
 
 #create an ARM deployment#
-New-AzResourceGroupDeployment -Name "deploy-storageaccount-010320" -ResourceGroupName $resourceGroup
+New-AzResourceGroupDeployment -Name "deploy-sa-010320" -ResourceGroupName $resourceGroup -TemplateFile $template -TemplateParameterFile $templateParam -Force
