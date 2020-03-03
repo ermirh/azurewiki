cc52632eda2076fd8b36108e2a7a4f92cc40bd95
ehoxhaj <hoxhajermir@outlook.com>
2020-03-03 17:01:19 +0100

    edited vm template

--- azure-virtualmachine-template/README.md
+++ azure-virtualmachine-template/README.md
@@ -1,3 +1,6 @@
 # Azure Virtual Machine ARM Template
 
 Deploy a Windows Server 2019 DC VM using ARM template.
+
+Reference resource provider documentation:  https://docs.microsoft.com/en-us/azure/templates/microsoft.compute/2019-07-01/virtualmachines
+
--- azure-virtualmachine-template/deploy.ps1
+++ azure-virtualmachine-template/deploy.ps1
@@ -1,21 +1,24 @@
 Connect-AzAccount
 
+#select your subscription where going to provision resources
 Get-AzSubscription -SubscriptionName '<sub_name>' | Select-AzSubscription
 
-#create new resource group - or use already an existing one (Get-AzResourceGroup)#
-$resourceGroup = "wikirg0203"
+#create a variable for Resource Group (Get-AzResourceGroup)
+$resourceGroup = ""
 
 #enter username & password for VM credentials
 $credentials = Get-Credential
+
+#store user & password to pass values into arm template deployment
 $adminUser = $credentials.UserName
 $adminPassword = $credentials.Password
 
-#local path of the template & parameters file
+#specify local path of the template & parameters file
 $templateParam = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-virtualmachine-template\template.parameters.json"
 $template = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-virtualmachine-template\template.json"
 
-#test the deployment - empty output is ok, working#
+#test the deployment - empty output is ok, working
 Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateParameterFile $templateParam -TemplateFile $template -adminUsername $adminUser -adminPassword $adminPassword
 
-#create an ARM deployment#
+#create an ARM deployment
 New-AzResourceGroupDeployment -Name "deploy-vm-030320" -ResourceGroupName $resourceGroup -TemplateFile $template -TemplateParameterFile $templateParam -adminUsername $adminUser -adminPassword $adminPassword -Force
