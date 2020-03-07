9473b501cb7f249eb64d9fde6912787f867070cd
ehoxhaj <hoxhajermir@outlook.com>
2020-03-07 19:19:27 +0100

    sql template

---
+++ azure-sql-template/README.md
@@ -0,0 +1,6 @@
+# Azure SQL Server ARM template deployment #
+
+Create a SQL Server with customer-managed keys using Key Vaul and adding firewall rule to access this sql server.
+
+Official SQL api documentation: https://docs.microsoft.com/en-us/azure/templates/microsoft.sql/2015-05-01-preview/servers
+
---
+++ azure-sql-template/deploy.ps1
@@ -0,0 +1,24 @@
+Connect-AzAccount
+
+#select your subscription where going to provision resources
+Get-AzSubscription -SubscriptionName 'Azure subscription 1' | Select-AzSubscription
+
+#create a variable for Resource Group (Get-AzResourceGroup)
+$resourceGroup = "cmk-demo-rg001"
+
+#enter username & password for SQL server credentials
+$credentials = Get-Credential
+
+#store user & password to pass values into arm template deployment
+$sqlUser = $credentials.UserName
+$sqlUserPassword = $credentials.Password
+
+#specify local path of the template & parameters file
+$templateParam = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-sql-template\template.parameters.json"
+$template = "C:\Users\ehoxhaj\Desktop\Box Sync\azurewiki\azure-sql-template\template.json"
+
+#test the deployment - empty output is ok, working
+Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateParameterFile $templateParam -TemplateFile $template -sqlServerAdministrator $sqlUser -sqlServerAdministratorPwd $sqlUserPassword
+
+#create an ARM deployment
+New-AzResourceGroupDeployment -Name "deploy-sql-070320" -ResourceGroupName $resourceGroup -TemplateFile $template -TemplateParameterFile $templateParam -sqlServerAdministrator $sqlUser -sqlServerAdministratorPwd $sqlUserPassword -Force
